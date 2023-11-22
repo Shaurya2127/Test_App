52e875d218f2707f6d18527b79251fc99e990401
Shaurya2127 <shauryachauhan721@gmail.com>
2023-11-22 18:06:42 +0530

    new files made

---
+++ Python/Python_Notebooks/new.py
@@ -0,0 +1,11 @@
+a = 10 
+b = 15 
+c = 12
+if a == b :
+     print ("first condition is true")
+elif a == c :
+     print("secong condition is ture")
+else :
+     print("the arguments are wrongs")
+
+
