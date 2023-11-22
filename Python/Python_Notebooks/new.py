6a3ac47184dad0e318bb5b01e21ce689f18226a9
Shaurya2127 <shauryachauhan721@gmail.com>
2023-11-22 18:14:06 +0530

    few changes

--- Python/Python_Notebooks/new.py
+++ Python/Python_Notebooks/new.py
@@ -2,10 +2,10 @@ a = 10
 b = 15 
 c = 12
 if a == b :
-     print ("first condition is true")
+     print ("first condition is true.")
 elif a == c :
-     print("secong condition is ture")
+     print("secong condition is ture.")
 else :
-     print("the arguments are wrongs")
+     print("the arguments are wrong.")
 
 
